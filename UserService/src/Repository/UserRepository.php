46ecac39f523175c49ee48fb094cad646fcfbb1d
Felobateer <felobateersamy01@gmail.com>
2025-01-12 18:38:42 +0100

    complete user microservice

--- UserService/src/Repository/UserRepository.php
+++ UserService/src/Repository/UserRepository.php
@@ -6,9 +6,6 @@ use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-/**
- * @extends ServiceEntityRepository<User>
- */
 class UserRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
@@ -16,36 +13,49 @@ class UserRepository extends ServiceEntityRepository
         parent::__construct($registry, User::class);
     }
 
-    /**
-     * Finds users by a specific field and returns an array of User objects.
-     *
-     * @param mixed $value The value to search for.
-     * @return User[] Returns an array of User objects.
-     */
-    public function findByExampleField(mixed $value): array
+    // Find a user by their ID
+    public function findById(int $id): ?User
     {
-        return $this->createQueryBuilder('u')
-            ->andWhere('u.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('u.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult();
+        return $this->find($id);
     }
 
-    /**
-     * Finds a single user by a specific field or returns null if not found.
-     *
-     * @param mixed $value The value to search for.
-     * @return User|null Returns a User object or null.
-     */
-    public function findOneBySomeField(mixed $value): ?User
+    // Find a user by their email
+    public function findByEmail(string $email): ?User
     {
-        return $this->createQueryBuilder('u')
-            ->andWhere('u.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult();
+        return $this->findOneBy(['email' => $email]);
+    }
+
+    // Get all users
+    public function findAllUsers(): array
+    {
+        return $this->findAll();
+    }
+
+    // Update user details
+    public function updateUser(User $user, array $data): User
+    {
+        if (isset($data['firstName'])) {
+            $user->setFirstName($data['firstName']);
+        }
+        if (isset($data['lastName'])) {
+            $user->setLastName($data['lastName']);
+        }
+        if (isset($data['email'])) {
+            $user->setEmail($data['email']);
+        }
+        if (isset($data['address'])) {
+            $user->setAddress($data['address']);
+        }
+        if (isset($data['phoneNumber'])) {
+            $user->setPhoneNumber($data['phoneNumber']);
+        }
+        if (isset($data['dateOfBirth'])) {
+            $user->setDateOfBirth(new \DateTime($data['dateOfBirth']));
+        }
+
+        $this->_em->persist($user);
+        $this->_em->flush();
+
+        return $user;
     }
 }
-
